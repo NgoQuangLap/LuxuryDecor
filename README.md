9e276621c4f776e9dbddc6496c295102b9c69832
quannn <quannn@vmodev.com>
2023-05-03 17:59:12 +0700

    Update code do an tot nghiep

---
+++ README.md
@@ -0,0 +1,12 @@
+# Do an tot nghiep LuxuryDecor
+# Description 
+Project: Website LuxuryDecor la do an tot nghiep khoa hoc may tinh viet bang java spring boot, spring mvc va co ung dung luat khai pha 
+ket hop trong chuc nang goi y san pham di kem
+
+# Technology
+- HTML
+- CSS
+- JS
+- Java JDK 13
+- Spring MVC
+- GIT
---
+++ src/test/java/com/luxuryshop/GeneratePassword.java
@@ -0,0 +1,11 @@
+package com.luxuryshop;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeneratePassword {
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(5);
+		String result = encoder.encode("123456");
+		System.out.println(result);
+	}
+}
